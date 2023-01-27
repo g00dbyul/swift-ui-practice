45cf09f675df56bfcc2f20ae90f429d1aa076a83
g00dbyul <skypower4203@gmail.com>
2023-01-27 15:04:00 +0900

    Initial Commit

---
+++ swift-ui-alert/swift_ui_alertApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_alertApp.swift
+//  swift-ui-alert
+//
+//  Created by Godbyul on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_alertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
